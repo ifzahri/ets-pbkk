0ae7da85c289ac8bb5598b390058b116c6c63e69
Iftala <circlebytes.iftala@gmail.com>
2024-10-16 13:02:44 +0700

    feat: delete functionalty removed in dokters

--- app/Http/Controllers/DokterController.php
+++ app/Http/Controllers/DokterController.php
@@ -15,10 +15,10 @@ class DokterController extends Controller
 
         $status = $request->input('status');
         $search = $request->input('search');
-        
+
         $query = Konsultasi::where('dokter_id', $dokter->id)
             ->with('pasien');
-        
+
         if ($status) {
             $query->where('status', $status);
         }
@@ -29,35 +29,35 @@ class DokterController extends Controller
         }
 
         $konsultasis = $query->paginate(10);
-    
+
         return view('dokter.index', compact('dokter', 'konsultasis', 'status', 'search'));
     }
-    
+
     public function show($id)
     {
         $dokter = Dokter::where('email', Auth::user()->email)->firstOrFail();
-        
+
         $konsultasi = Konsultasi::where('id', $id)
             ->where('dokter_id', $dokter->id)
             ->with('pasien')
             ->firstOrFail();
-        
+
         return view('dokter.show', compact('dokter', 'konsultasi'));
     }
 
     public function addMedication(Request $request, $id)
-{
-    $dokter = Dokter::where('email', Auth::user()->email)->firstOrFail();
-    
-    $konsultasi = Konsultasi::where('id', $id)
-        ->where('dokter_id', $dokter->id)
-        ->firstOrFail();
-    
-    $konsultasi->update([
-        'penanganan' => $request->penanganan,
-        'status' => 'diterima'
-    ]);
-    
-    return redirect()->route('dokter.index')->with('success', 'Medication added and status updated to accepted.');
-}
+    {
+        $dokter = Dokter::where('email', Auth::user()->email)->firstOrFail();
+
+        $konsultasi = Konsultasi::where('id', $id)
+            ->where('dokter_id', $dokter->id)
+            ->firstOrFail();
+
+        $konsultasi->update([
+            'penanganan' => $request->penanganan,
+            'status' => 'diterima'
+        ]);
+
+        return redirect()->route('dokter.index')->with('success', 'Medication added and status updated to accepted.');
+    }
 }
